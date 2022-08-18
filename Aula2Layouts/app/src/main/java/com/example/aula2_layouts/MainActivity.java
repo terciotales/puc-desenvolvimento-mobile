8b470e122cd3ee87f192ef6c092f69039cfa1cd8
Tércio Tales Barnabé <terciotalesb@gmail.com>
2022-08-17 21:49:55 -0300

    Arquivos finais da aula 2

--- Aula2Layouts/app/src/main/java/com/example/aula2_layouts/MainActivity.java
+++ Aula2Layouts/app/src/main/java/com/example/aula2_layouts/MainActivity.java
@@ -2,16 +2,50 @@ package com.example.aula2_layouts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     String nome;
+    Button btn01;
+    EditText num1;
+    EditText num2;
+    TextView sumtext;
+
+    String soma;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.novo_layout);
 
+        btn01 = findViewById(R.id.btn1);
+        num1 = findViewById(R.id.num1);
+        num2 = findViewById(R.id.num2);
+        sumtext = findViewById(R.id.sumtext);
+
+        btn01.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+
+                soma = num1.getText().toString();
+                soma += " " + num2.getText().toString();
+                sumtext.setText(soma);
+
+                Intent i = new Intent(MainActivity.this, SegundaAtividade.class);
+
+                startActivity(i);
+
+//                Toast.makeText(MainActivity.this, "A soma dos números é: " + soma, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
---
+++ Aula2Layouts/app/src/main/java/com/example/aula2_layouts/SegundaAtividade.java
@@ -0,0 +1,15 @@
+package com.example.aula2_layouts;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SegundaAtividade extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_main);
+    }
+}
