0798ff0fe6baf1a3b7b511c90d88cac732583b50
Tércio Tales Barnabé <terciotalesb@gmail.com>
2022-08-21 14:29:51 -0300

    Adiciona arquivos da terceira aula

--- Aula2Layouts/app/src/main/java/com/example/aula2_layouts/MainActivity.java
+++ Aula2Layouts/app/src/main/java/com/example/aula2_layouts/MainActivity.java
@@ -27,24 +27,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.novo_layout);
 
         btn01 = findViewById(R.id.btn1);
-        num1 = findViewById(R.id.num1);
-        num2 = findViewById(R.id.num2);
-        sumtext = findViewById(R.id.sumtext);
 
         btn01.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View view) {
-
-                soma = num1.getText().toString();
-                soma += " " + num2.getText().toString();
-                sumtext.setText(soma);
-
                 Intent i = new Intent(MainActivity.this, SegundaAtividade.class);
-
                 startActivity(i);
-
-//                Toast.makeText(MainActivity.this, "A soma dos números é: " + soma, Toast.LENGTH_LONG).show();
             }
         });
     }
