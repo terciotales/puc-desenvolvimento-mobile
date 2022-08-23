dd972e4dfcfa5e97e4331f8abc2fd6af63f023e7
Tércio Tales Barnabé <terciotalesb@gmail.com>
2022-08-23 19:50:43 -0300

    Adiciona arquivos da atividade 01

---
+++ Atividade01/app/src/main/java/com/example/atividade01/SegundaTela.java
@@ -0,0 +1,13 @@
+package com.example.atividade01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;;
+
+public class SegundaTela extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.segunda_tela);
+    }
+}
