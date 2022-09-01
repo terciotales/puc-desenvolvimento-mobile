5f9c5b97d1e12c897a53fd3da9c8c581e29d075a
Tércio Tales Barnabé <terciotalesb@gmail.com>
2022-08-31 21:31:19 -0300

    Arquivos finais da aula 05

--- Aula05BuscaEIntents/app/src/main/java/com/example/aula05buscaeintents/MainActivity.java
+++ Aula05BuscaEIntents/app/src/main/java/com/example/aula05buscaeintents/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.aula05buscaeintents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -16,33 +19,62 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText busca;
     ListView lista;
     EditText ed01;
     Button btn;
     Button btn_delete;
+
+    ArrayAdapter adapter;
+    ArrayList<String> nomes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        busca = findViewById(R.id.busca);
         lista = findViewById(R.id.lista);
         ed01 = findViewById(R.id.ed01);
         btn = findViewById(R.id.btn01);
         btn_delete = findViewById(R.id.btn_delete);
 
-        ArrayList<String> nomes = new ArrayList<>();
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, nomes);
+        MainActivity.this.nomes.add("Humberto Gessinger - 62");
+        MainActivity.this.nomes.add("Billie Eilish - 61");
+        MainActivity.this.nomes.add("Cristiano Ronaldo - 99");
+        MainActivity.this.nomes.add("Mateus França - 26564");
+        MainActivity.this.nomes.add("Sadio Mané - 5485451");
+
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, MainActivity.this.nomes);
+
+        lista.setAdapter(MainActivity.this.adapter);
+
+        busca.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                MainActivity.this.adapter.getFilter().filter(charSequence);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String text = ed01.getText().toString();
 
-                if (!text.isEmpty() && !nomes.contains(text)) {
-                    nomes.add(text);
+                if (!text.isEmpty() && !MainActivity.this.nomes.contains(text)) {
+                    MainActivity.this.nomes.add(text);
 
-                    lista.setAdapter(adapter);
+                    lista.setAdapter(MainActivity.this.adapter);
 
                     ed01.setText("");
 
@@ -60,7 +92,14 @@ public class MainActivity extends AppCompatActivity {
         lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(MainActivity.this, "Clicou em " + nomes.get(i), Toast.LENGTH_SHORT).show();
+                String contato = MainActivity.this.nomes.get(i);
+                String nome = contato.split("-")[0];
+                String telefone = contato.split("-")[1];
+                Intent j = new Intent(MainActivity.this, SegundaTela.class);
+                j.putExtra("nome", nome);
+                j.putExtra("telefone", telefone);
+                startActivity(j);
+                finish();
             }
         });
 
@@ -68,7 +107,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if (lista.getCount() > 0) {
-                    adapter.clear();
+                    MainActivity.this.adapter.clear();
                 }
             }
         });
--- Aula05BuscaEIntents/app/src/main/java/com/example/aula05buscaeintents/SegundaTela.java
+++ Aula05BuscaEIntents/app/src/main/java/com/example/aula05buscaeintents/SegundaTela.java
@@ -2,13 +2,39 @@ package com.example.aula05buscaeintents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SegundaTela extends AppCompatActivity {
+
+    TextView nome, telefone;
+    Button voltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda_tela);
+        nome = findViewById(R.id.textView);
+        telefone = findViewById(R.id.textView2);
+        voltar = findViewById(R.id.button);
+
+        Intent dadosRecebidos = getIntent();
+
+        nome.setText("Nome: " + dadosRecebidos.getStringExtra("nome"));
+        telefone.setText("Nome: " + dadosRecebidos.getStringExtra("telefone"));
+
+        voltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(SegundaTela.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+
     }
 }
