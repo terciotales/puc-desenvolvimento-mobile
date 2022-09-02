2a45bd271576761472b2e41e68b54330fa0a5d94
Tércio Tales Barnabé <terciotalesb@gmail.com>
2022-09-02 19:04:10 -0300

    Adiciona arquivos iniciais da aula 06

--- Atividade01/app/src/main/java/com/example/atividade01/MainActivity.java
+++ Atividade01/app/src/main/java/com/example/atividade01/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.atividade01;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
--- Atividade01/app/src/main/java/com/example/atividade01/SegundaTela.java
+++ Atividade01/app/src/main/java/com/example/atividade01/SegundaTela.java
@@ -2,12 +2,44 @@ package com.example.atividade01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+;import java.util.Random;
 
 public class SegundaTela extends AppCompatActivity {
+    Button button;
+    Button button1;
+    TextView textView;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.segunda_tela);
+
+        button = findViewById(R.id.button);
+        button1 = findViewById(R.id.button2);
+        textView = findViewById(R.id.rndNum);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random rand = new Random();
+                int random_number = rand.nextInt(100);
+                String random = String.valueOf(random_number);
+                textView.setText(random);
+            }
+        });
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, SegundaTela.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
