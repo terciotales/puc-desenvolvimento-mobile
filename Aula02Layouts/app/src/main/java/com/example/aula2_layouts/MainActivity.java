a4059aa7d36ce86a8ac214dfe66db48b61b98d63
Tércio Tales Barnabé <terciotalesb@gmail.com>
2022-09-01 21:22:02 -0300

    Finaliza atividade 01

--- Aula02Layouts/app/src/main/java/com/example/aula2_layouts/MainActivity.java
+++ Aula02Layouts/app/src/main/java/com/example/aula2_layouts/MainActivity.java
@@ -13,27 +13,39 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    String nome;
     Button btn01;
-    EditText num1;
-    EditText num2;
+    Button btn02;
+    EditText txt01;
+    EditText txt02;
     TextView sumtext;
 
-    String soma;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.novo_layout);
 
+        txt01 = findViewById(R.id.txt01);
+        txt02 = findViewById(R.id.txt02);
         btn01 = findViewById(R.id.btn1);
+        btn02 = findViewById(R.id.btn2);
+        sumtext = findViewById(R.id.sumtext);
 
         btn01.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View view) {
+                String text = txt01.getText().toString() + txt02.getText().toString();
+                sumtext.setText(text);
+            }
+        });
+
+        btn02.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
                 Intent i = new Intent(MainActivity.this, SegundaAtividade.class);
                 startActivity(i);
+                finish();
             }
         });
     }
--- Aula02Layouts/app/src/main/java/com/example/aula2_layouts/SegundaAtividade.java
+++ Aula02Layouts/app/src/main/java/com/example/aula2_layouts/SegundaAtividade.java
@@ -1,15 +1,48 @@
 package com.example.aula2_layouts;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import java.util.Random;
+
 
 public class SegundaAtividade extends AppCompatActivity {
+
+    Button button;
+    Button button1;
+    TextView textView;
+
+
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        button1 = findViewById(R.id.button2);
+        textView = findViewById(R.id.rndNum);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random rand = new Random();
+                int random_number = rand.nextInt(100);
+                String random = String.valueOf(random_number);
+                textView.setText(random);
+            }
+        });
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(SegundaAtividade.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
