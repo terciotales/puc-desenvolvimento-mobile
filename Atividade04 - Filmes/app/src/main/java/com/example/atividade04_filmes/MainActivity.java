bdea19963b1e1db42e772c404c8dd0cd31e05eac
Tércio Tales Barnabé <terciotalesb@gmail.com>
2022-09-17 19:53:15 -0300

    Arquivos iniciais atividade aula 04

---
+++ Atividade04 - Filmes/app/src/main/java/com/example/atividade04_filmes/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.atividade04_filmes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
